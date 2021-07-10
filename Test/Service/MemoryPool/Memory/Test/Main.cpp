bb3a59d2f68572e0647998769df84f776083f0ee
Konstantin Tomashevich <ekit.mail@gmail.com>
2021-07-10 16:41:33 +0300

    Port MemoryPool service tests to Testing service.

---
+++ Test/Service/MemoryPool/Memory/Test/Main.cpp
@@ -0,0 +1,6 @@
+#include <Memory/Test/Pool.hpp>
+
+#include <Testing/SetupMain.hpp>
+
+// Use "include marker", so linker will not omit translation units with autoregistered tests.
+static const bool poolTestIncludeMarker = Emergence::Memory::Test::PoolTestIncludeMarker ();
