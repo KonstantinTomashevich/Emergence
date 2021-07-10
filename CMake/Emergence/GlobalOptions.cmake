45518dbd7107832be29fbbe7fec6343f7687ab41
Konstantin Tomashevich <ekit.mail@gmail.com>
2021-07-10 17:35:10 +0300

    CMake: Add EMERGENCE_ENABLE_COVERAGE option.

--- CMake/Emergence/GlobalOptions.cmake
+++ CMake/Emergence/GlobalOptions.cmake
@@ -4,6 +4,7 @@ include_guard (GLOBAL)
 
 option (EMERGENCE_TREAT_WARNINGS_AS_ERRORS "Enables \"treat warnings as errors\" compiler policy for all targets." ON)
 option (EMERGENCE_COMPILE_TIME_TRACE "Requests compiler to print time trace. Supported only on CLang." OFF)
+option (EMERGENCE_ENABLE_COVERAGE "Add compile and link time flags, that enable code coverage reporting." OFF)
 
 if (EMERGENCE_TREAT_WARNINGS_AS_ERRORS)
     if (MSVC)
@@ -19,5 +20,14 @@ if (EMERGENCE_COMPILE_TIME_TRACE)
     else ()
         # TODO: Alternatives for other compilers?
         message (FATAL_ERROR "Currently time tracing is supported only for CLang.")
+    endif ()
+endif ()
+
+if (EMERGENCE_ENABLE_COVERAGE)
+    if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "^.*Clang$")
+        add_compile_options (-fprofile-instr-generate -fcoverage-mapping)
+    else ()
+        add_compile_options (--coverage)
+        add_link_options (--coverage)
     endif ()
 endif ()
