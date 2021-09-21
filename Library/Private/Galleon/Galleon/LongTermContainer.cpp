8447944e1887aff148d1f6369e088cf86682e249
Konstantin Tomashevich <ekit.mail@gmail.com>
2021-09-21 20:24:15 +0300

    #14. CheckCodeStyle: Try fixing includes and using clang 12.

--- Library/Private/Galleon/Galleon/LongTermContainer.cpp
+++ Library/Private/Galleon/Galleon/LongTermContainer.cpp
@@ -1,3 +1,5 @@
+#include <cstring>
+
 #include <Galleon/CargoDeck.hpp>
 #include <Galleon/LongTermContainer.hpp>
 
--- Test/Service/Warehouse/Shared/Warehouse/Test/Scenario.cpp
+++ Test/Service/Warehouse/Shared/Warehouse/Test/Scenario.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstring>
 #include <optional>
 #include <sstream>
 #include <unordered_map>
