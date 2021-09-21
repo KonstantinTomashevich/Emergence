b61c7590cf60c3efe8c1a02dd18302780e36d3f4
Konstantin Tomashevich <ekit.mail@gmail.com>
2021-09-21 20:09:34 +0300

    #14. CheckCodeStyle: Try switch back to Ubuntu build.

--- Script/CodeStyle/CheckCLangFormat.ps1
+++ Script/CodeStyle/CheckCLangFormat.ps1
@@ -1,12 +1,17 @@
 #!/usr/bin/env pwsh
 
-if ($args.Count -ne 0)
+if ($args.Count -gt 1)
 {
-    echo "Usage: <script>"
+    echo "Usage: <script> [clang-format-version-major]"
     exit -1
 }
 
 $CLangFormatExecutable = "clang-format"
+if ($args.Count -eq 1)
+{
+    $CLangFormatExecutable += "-" + $args[0]
+}
+
 if (-Not(Get-Command $CLangFormatExecutable))
 {
     echo "Unable to find $CLangFormatExecutable in path!"
