2eb64e74937015e95b1bfa9b595e97e00d016c4d
Konstantin Tomashevich <ekit.mail@gmail.com>
2021-10-31 18:55:40 +0300

    TopDownShooter: Simple test scene and initial assets.

--- Executable/TopDownShooter/Game/StubMain.cpp
+++ Executable/TopDownShooter/Game/StubMain.cpp
@@ -1,11 +1,71 @@
 #include <OgreApplicationContext.h>
+#include <OgreEntity.h>
+#include <OgreRenderWindow.h>
 #include <OgreRoot.h>
+#include <OgreTimer.h>
 
 int main (int /*unused*/, char ** /*unused*/)
 {
     OgreBites::ApplicationContext application {"TopDownShooter"};
     application.initApp ();
-    application.getRoot ()->startRendering ();
+
+    Ogre::Root *root = application.getRoot ();
+    Ogre::SceneManager *sceneManager = root->createSceneManager ();
+
+    Ogre::RTShader::ShaderGenerator *shaderGenerator = Ogre::RTShader::ShaderGenerator::getSingletonPtr ();
+    shaderGenerator->addSceneManager (sceneManager);
+
+    Ogre::Light *light = sceneManager->createLight ("MainLight");
+    light->setType (Ogre::Light::LT_DIRECTIONAL);
+    light->setSpecularColour (Ogre::ColourValue::White);
+
+    Ogre::SceneNode *lightNode = sceneManager->getRootSceneNode ()->createChildSceneNode ();
+    lightNode->lookAt ({0.0f, -2.0f, 0.0f}, Ogre::Node::TS_LOCAL);
+    lightNode->attachObject (light);
+
+    Ogre::Camera *camera = sceneManager->createCamera ("MainCamera");
+    camera->setNearClipDistance (0.5f);
+    camera->setAutoAspectRatio (true);
+
+    Ogre::SceneNode *cameraNode = sceneManager->getRootSceneNode ()->createChildSceneNode ();
+    cameraNode->setPosition (0, 5, 5);
+    cameraNode->lookAt ({0.0f, 0.0f, -1.0f}, Ogre::Node::TS_WORLD);
+    cameraNode->attachObject (camera);
+
+    for (int x = -4; x <= 4; ++x)
+    {
+        for (int z = -4; z <= 4; ++z)
+        {
+            const bool placeWall = x == -4 || x == 4 || z == -4;
+            Ogre::Entity *tile = sceneManager->createEntity (placeWall ? "Wall.mesh" : "FloorTile.mesh");
+
+            Ogre::SceneNode *tileNode = sceneManager->getRootSceneNode ()->createChildSceneNode ();
+            tileNode->setPosition (static_cast<float> (x), 0.0f, static_cast<float> (z));
+            tileNode->attachObject (tile);
+        }
+    }
+
+    Ogre::Entity *player = sceneManager->createEntity ("Player.mesh");
+    Ogre::SceneNode *playerNode = sceneManager->getRootSceneNode ()->createChildSceneNode ();
+    playerNode->setPosition (0.0f, 0.0f, 0.0f);
+    playerNode->attachObject (player);
+
+    application.getRenderWindow ()->addViewport (camera);
+
+    while (!application.getRoot ()->endRenderingQueued ())
+    {
+        constexpr const uint64_t msPeriod = 3000u;
+        const uint64_t msLocal = application.getRoot ()->getTimer ()->getMilliseconds () % msPeriod;
+        const float angle = Ogre::Math::PI * 2.0f * (static_cast<float> (msLocal) / static_cast<float> (msPeriod));
+
+        Ogre::Vector3 lookTarget {cos (angle), -1.0f, sin (angle)};
+        lightNode->lookAt (lookTarget, Ogre::Node::TS_WORLD);
+
+        playerNode->setPosition (cos(angle) * 2.0f, 0.0f, sin(angle) * 2.0f);
+
+        application.getRoot ()->renderOneFrame ();
+    }
+
     application.closeApp ();
     return 0u;
 }
