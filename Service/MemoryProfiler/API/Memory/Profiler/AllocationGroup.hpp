44a5ff17e4159ed7f49a0d0b3f5d9371393606a3
Konstantin Tomashevich <ekit.mail@gmail.com>
2022-01-12 16:15:29 +0300

    #22. MemoryProfiler:: Add documentation.

--- Service/MemoryProfiler/API/Memory/Profiler/AllocationGroup.hpp
+++ Service/MemoryProfiler/API/Memory/Profiler/AllocationGroup.hpp
@@ -10,14 +10,16 @@
 
 namespace Emergence::Memory::Profiler
 {
+/// \brief Logical group for tracking one or more logically connected allocators. Might be a subgroup of another group.
+/// \details Grouping makes it easy to distinguish most aggressive memory consumers in program.
+///          Group hierarchy provides easy way to go from "big picture" view to details and vice versa.
+///          For example, it allows to descend from statement "Records eat much more memory than they should"
+///          to "There is too much indices for record types X and Y" without any additional debugging or code edition.
 class AllocationGroup final
 {
 public:
-    // NOTE: Stack is tread local to avoid concurrent push/pop operations.
-    // NOTE: AllocationGroup construction and destruction must be thread safe.
-    // NOTE: AllocationGroup construction breaks children iterators.
-    // NOTE: GroupA -> GroupB -> GroupA -- valid stack.
-
+    /// \brief RAII object for AllocationGroup stack management.
+    /// \see AllocationGroup::Top
     class PlacedOnStack final
     {
     public:
@@ -36,6 +38,7 @@ public:
         explicit PlacedOnStack (void *_groupHandle) noexcept;
     };
 
+    /// \brief Provides iteration over AllocationGroup children.
     class Iterator final
     {
     public:
@@ -50,49 +53,90 @@ public:
         explicit Iterator (const std::array<uint8_t, DATA_MAX_SIZE> *_data) noexcept;
     };
 
+    /// \return Root group of the AllocationGroup tree. Always exists.
+    /// \details Do not explicitly make ::Root parent of new allocation groups unless it is truly necessary,
+    ///          because it blocks module users from incorporating module groups into bigger application tree through
+    ///          allocation group stack.
     static AllocationGroup Root () noexcept;
 
+    /// \return Top group on thread local group stack.
+    /// \details Group stack makes it easy to incorporate allocation group structure of local modules into
+    ///          allocation group tree of entire application by avoiding explicit specification of group
+    ///          parent, which makes these groups children of the ::Top group. For example we may incorporate
+    ///          several different containers, like RecordCollection, into application-level world, like Registry.
+    ///          Stack is thread local, because it's impossible to imagine how shared stack should work
+    ///          and for which tasks this approach is really needed.
     static AllocationGroup Top () noexcept;
 
     /// \brief Creates empty placeholder allocation group, that has zero logic.
+    /// \details Thread safe.
     AllocationGroup () noexcept;
 
     /// \brief Creates new group with given id, that is child of ::Top.
+    /// \details If such group is already exists, nothing is overwritten.
+    ///          Thread safe. Invalidates iterators of parent group.
     explicit AllocationGroup (UniqueString _id) noexcept;
 
     /// \brief Constructs allocation group as child of other given allocation group.
+    /// \details If such group is already exists, nothing is overwritten.
+    ///          Thread safe. Invalidates iterators of parent group.
     AllocationGroup (const AllocationGroup &_parent, UniqueString _id) noexcept;
 
+    /// \details Thread safe.
     AllocationGroup (const AllocationGroup &_other) noexcept;
 
+    /// \details Thread safe.
     AllocationGroup (AllocationGroup &&_other) noexcept;
 
+    /// \details Thread safe.
     ~AllocationGroup () noexcept;
 
+    /// \brief Places this group on top of thread local stack.
+    /// \return RAII object for stack push/pop operations.
+    /// \see ::Top
     [[nodiscard]] PlacedOnStack PlaceOnTop () const noexcept;
 
+    /// \brief Record allocation of given amount of bytes.
+    /// \details Allocated bytes are reserved for this group, but not yet used by any object.
+    ///          Thread safe.
     void Allocate (size_t _bytesCount) noexcept;
 
+    /// \brief Record acquisition of given amount of bytes for usage.
+    /// \invariant ::GetReserved >= _bytesCount
+    /// \details Thread safe.
     void Acquire (size_t _bytesCount) noexcept;
 
+    /// \brief Record that given amount of bytes is no longer used by any object.
+    /// \invariant ::GetAcquired >= _bytesCount
+    /// \details Thread safe.
     void Release (size_t _bytesCount) noexcept;
 
+    /// \brief Record that given amount of bytes is no longer owned by this group.
+    /// \invariant ::GetReserved >= _bytesCount
+    /// \details Thread safe.
     void Free (size_t _bytesCount) noexcept;
 
+    /// \return Allocation group, that contains this group as subgroup.
+    /// \details Parent of ::Root is empty placeholder group, which in turn is parent of itself.
     [[nodiscard]] AllocationGroup Parent () const noexcept;
 
     [[nodiscard]] Iterator BeginChildren () const noexcept;
 
     [[nodiscard]] Iterator EndChildren () const noexcept;
 
+    /// \return Id of this group, unique among children of its ::Parent.
     [[nodiscard]] UniqueString GetId () const noexcept;
 
+    /// \return Amount of bytes, that are used by any object.
     [[nodiscard]] size_t GetAcquired () const noexcept;
 
+    /// \return Amount of bytes, that are not used by any object, but reserved for usage in future.
     [[nodiscard]] size_t GetReserved () const noexcept;
 
+    /// \return Total amount of bytes, that are owned by allocation group.
     [[nodiscard]] size_t GetTotal () const noexcept;
 
+    /// \details Guaranteed to be unique, but may change in every execution.
     [[nodiscard]] uintptr_t Hash () const noexcept;
 
     AllocationGroup &operator= (const AllocationGroup &_other) noexcept;
--- Service/MemoryProfiler/API/Memory/Profiler/Capture.hpp
+++ Service/MemoryProfiler/API/Memory/Profiler/Capture.hpp
@@ -12,35 +12,57 @@
 
 namespace Emergence::Memory::Profiler
 {
+/// \brief Enumerates all types of Event.
 enum class EventType
 {
+    /// \brief Memory is allocated for future usage.
     ALLOCATE = 0,
+
+    /// \brief Memory is being used for something.
     ACQUIRE,
+
+    /// \brief Memory is no longer used.
     RELEASE,
+
+    /// \brief Memory is no longer owned.
     FREE,
+
+    /// \brief Message, used to make capture events array more readable.
     MARKER
 };
 
+/// \brief Every memory management operation produces appropriate event.
 struct Event final
 {
     EventType type = EventType::ALLOCATE;
 
+    /// \brief Allocation group, associated with this event.
     AllocationGroup group;
 
+    /// \brief Time point, at which event occurred.
     std::chrono::time_point<std::chrono::high_resolution_clock> time;
 
     union
     {
+        /// \brief Amount of bytes, used by operation, associated with ::type.
+        /// \invariant ::type is EventType::ALLOCATE, EventType::ACQUIRE, EventType::RELEASE or EventType::Free.
         size_t bytes = 0u;
+
+        /// \brief Marker id.
+        /// \invariant ::type is EventType::MARKER.
         UniqueString markerId;
     };
 };
 
+/// \brief Creates new EventType::MARKER event, associated with given group.
+/// \details Thread safe.
 void AddMarker (UniqueString _markerId, AllocationGroup _group = AllocationGroup::Root ()) noexcept;
 
+/// \brief State of allocation group, that was captured during Capture::Start.
 class CapturedAllocationGroup final
 {
 public:
+    /// \brief Provides iteration over captured states of allocation group children.
     class Iterator final
     {
     public:
@@ -65,12 +87,16 @@ public:
 
     [[nodiscard]] Iterator EndChildren () const noexcept;
 
+    /// \return Id of this group, unique among its siblings.
     [[nodiscard]] UniqueString GetId () const noexcept;
 
+    /// \return Amount of bytes, that are used by any object.
     [[nodiscard]] size_t GetAcquired () const noexcept;
 
+    /// \return Amount of bytes, that are not used by any object, but reserved for usage in future.
     [[nodiscard]] size_t GetReserved () const noexcept;
 
+    /// \return Total amount of bytes, that are owned by allocation group.
     [[nodiscard]] size_t GetTotal () const noexcept;
 
     EMERGENCE_DELETE_ASSIGNMENT (CapturedAllocationGroup);
@@ -83,6 +109,7 @@ private:
     explicit CapturedAllocationGroup (void *_handle) noexcept;
 };
 
+/// \brief Allows user to read memory management events that occurred after Capture::Start.
 class EventObserver
 {
 public:
@@ -93,6 +120,8 @@ public:
 
     ~EventObserver () = default;
 
+    /// \return Next event or `nullptr` if there is no new events.
+    /// \details Thread safe.
     const Event *NextEvent () noexcept;
 
     /// Assigning captures seems counter-intuitive.
@@ -106,11 +135,15 @@ private:
     explicit EventObserver (std::array<uint8_t, DATA_MAX_SIZE> *_data) noexcept;
 };
 
+/// \brief Provides API for capturing memory consumption.
 class Capture final
 {
 public:
     Capture () = delete;
 
+    /// \brief Capture state of all existing allocation groups and create
+    ///        event observer for memory consumption changes tracking.
+    /// \details Thread safe.
     static std::pair<CapturedAllocationGroup, EventObserver> Start () noexcept;
 };
 } // namespace Emergence::Memory::Profiler
