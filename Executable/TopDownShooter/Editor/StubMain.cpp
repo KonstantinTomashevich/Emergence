36829a991bcc5469e1192337ac31341fbd156377
Konstantin Tomashevich <ekit.mail@gmail.com>
2022-01-29 17:05:31 +0300

    #23. TopDownShooter::Editor: Replace Ogre stub with Urho3D stub.

--- Executable/TopDownShooter/Editor/StubMain.cpp
+++ Executable/TopDownShooter/Editor/StubMain.cpp
@@ -1,15 +1,44 @@
 #include <SyntaxSugar/MuteWarnings.hpp>
 
 BEGIN_MUTING_WARNINGS
-#include <OgreApplicationContext.h>
-#include <OgreRoot.h>
+#include <Urho3D/Engine/Application.h>
 END_MUTING_WARNINGS
 
-int main (int /*unused*/, char ** /*unused*/)
+class EditorApplication : public Urho3D::Application
 {
-    OgreBites::ApplicationContext application {"TopDownShooter"};
-    application.initApp ();
-    application.getRoot ()->startRendering ();
-    application.closeApp ();
-    return 0u;
-}
+    URHO3D_OBJECT (EditorApplication, Application)
+
+public:
+    EditorApplication (Urho3D::Context *_context) : Application (_context)
+    {
+    }
+
+    EditorApplication (const EditorApplication &_other) = delete;
+
+    EditorApplication (EditorApplication &&_other) = delete;
+
+    ~EditorApplication () override = default;
+
+    void Setup () override
+    {
+        Application::Setup ();
+    }
+
+    void Start () override
+    {
+        Application::Start ();
+    }
+
+    void Stop () override
+    {
+        Application::Stop ();
+    }
+
+    EditorApplication &operator= (const EditorApplication &_other) = delete;
+
+    EditorApplication &operator= (EditorApplication &&_other) = delete;
+};
+
+BEGIN_MUTING_WARNINGS
+URHO3D_DEFINE_APPLICATION_MAIN (EditorApplication)
+END_MUTING_WARNINGS
